bd34d8582da37cc19ace3c2f20db91c44f049520
HH2099 <123923824+HH2099@users.noreply.github.com>
2025-08-07 15:10:22 +0800

    0807

---
+++ event/script_e.js
@@ -0,0 +1,38 @@
+const menuToggle = document.getElementById('menuToggle');
+const sidebar = document.getElementById('sidebar');
+
+menuToggle.addEventListener('click', () => {
+  sidebar.classList.toggle('active');
+});
+
+window.addEventListener("scroll", function () {
+  const sidebar = document.querySelector('.sidebar');
+  const scrollY = window.scrollY;
+  const scrollHeight = document.body.scrollHeight;
+  const viewportHeight = window.innerHeight;
+
+  const scrollPercent = (scrollY + viewportHeight) / scrollHeight;
+
+  const isDesktop = window.innerWidth >= 768;
+
+  if (isDesktop && scrollPercent > 0.93) {
+    sidebar.classList.add('hide');
+  } else {
+    sidebar.classList.remove('hide');
+  }
+});
+
+window.addEventListener("scroll", function () {
+  const image = document.getElementById('myImage');
+  const scrollY = window.scrollY;
+  const scrollHeight = document.body.scrollHeight;
+  const viewportHeight = window.innerHeight;
+
+  const scrollPercent = (scrollY + viewportHeight) / scrollHeight;
+
+  if (scrollPercent > 0.15) {
+    image.classList.add('visible');
+  } else {
+    image.classList.remove('visible'); // 回到小於10%，隱藏圖片
+  }
+});
